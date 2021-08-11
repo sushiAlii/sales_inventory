ac79d2e8fefdb2efa35607bff48ed4f7130c4cf0
sushiAlii <aliasgar.laut@gmail.com>
2021-08-11 21:10:41 +0800

    last login page form!

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -10,26 +10,22 @@
                 >
                     <v-card 
                         class="elevation-10"
-                        max-width="500px"
+                        max-width="400px"
+                        
                         dark
                     >
                         <v-row
                             class="mx-10"
                         >
                                 <v-col
-                                    class="ml-n4"
                                     cols="12"
-
+                                    md="12"
                                 >
-                                    <v-card-title
-                                    
-                                    >
+                                    <v-card-title class="mt-5 ml-n7">
                                         Welcome back!
                                     </v-card-title>
                                 </v-col>
-                                <v-col
-                                    cols="12"
-                                >
+                                
                                     <v-form class="" @submit.prevent="handleLogin">
                                         <v-row>
                                             <v-col
@@ -64,10 +60,15 @@
                                                 v-model="formData.password"
                                                 />
                                             </v-col>
+                                            <v-col
+                                                cols="12"
+                                                md="12"
+                                            >
+                                                <div class="text-end">
+                                                    <v-btn class="mb-10" color="white" light type="submit">SIGN IN</v-btn>
+                                                </div>
+                                            </v-col>
                                         </v-row>
-                                        <div class="text-center mt-3">
-                                            <v-btn class="my-10" color="white" light type="submit">SIGN IN</v-btn>
-                                        </div>  
                                     </v-form>
                                 </v-col>
                         </v-row>
@@ -118,11 +119,8 @@
     .login-page{
         width: 100%;
         height: 100%;
-        max-height: 100%;
-        margin: 0;
-        padding: 0;
         background-image: url("https://images.pexels.com/photos/4195409/pexels-photo-4195409.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
         background-size: 100% 100%;
-        background-repeat: no-repeat;
+        
     }
 </style>
